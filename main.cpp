36510f3f904c5074b639ae49f72eb2e2c5d95f27
telepatehticman <telepatheticman13@gmail.com>
2019-05-18 17:13:47 -0600

    figure out boost later

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "boost/asio.hpp"
+#include "boost/asio/ip/udp.hpp"
+#include "boost/asio/io_service.hpp"
 
 int main(int argc, char* argv[]) {
+    boost::asio::io_service io_service;
+    boost::asio::ip::udp::socket socket(io_service);
+    socket.open(boost::asio::ip::udp::v4());
     return 0;
 }
