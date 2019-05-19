cc6c2d8fb6376dc7dd3e03cb5938e6e560e11622
telepatehticman <telepatheticman13@gmail.com>
2019-05-18 18:49:07 -0600

    code for sending to server
    untested

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <cstring>
+
+#define PORT_OUT 1982
+#define PORT_IN 1983
+#define MAXLINE 1024
 
 int main(int argc, char* argv[]) {
+    int sockfd;
+    char buffer[MAXLINE];
+    //Needs to be converted to c string
+    std::string what("Whello");
+    struct sockaddr_in serv;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&serv, 0, sizeof(serv));
+
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(PORT_OUT);
+    serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int n, len;
+
+    sendto(sockfd, what.c_str(), strlen(what.c_str()), 0, (const struct sockaddr *) &serv, sizeof(serv));
+
+    close(sockfd);
+
     return 0;
 }
