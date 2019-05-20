b3a859e516d968c50b107fc6b803f494335024ca
telepatehticman <telepatheticman13@gmail.com>
2019-05-19 22:39:40 -0600

    update to unsigned things

--- Utility.hpp
+++ Utility.hpp
@@ -2,13 +2,19 @@
 #define FAULTYDISK_UTILITY_HPP
 
 #include <string>
+#include <cstring>
+
+#define R_LEN 37
+#define D_LEN 49
+#define W_LEN 48
+#define A_LEN 49
 
 namespace util{
-    char* createWrite(const std::string& file, const int& loc, const int& len, char* data);
+    unsigned char* createWrite(const std::string& file, const unsigned int& loc, const unsigned short& len, char* data);
 
     bool validWrite (char* ack);
 
-    char* createRead(const std::string& file, const int& loc);
+    unsigned char* createRead(const std::string& file, const int& loc);
 
     bool parseRead(char* data);
 }
