a4dfb0356c92c4e9f16863470cb9ce8af736d963
telepatehticman <telepatheticman13@gmail.com>
2019-05-21 00:14:39 -0600

    implement vectorize and devectorize

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,3 +1,28 @@
 #include "Algorithms.hpp"
 
+std::vector <char*> algo::vectorize(char *memblock, const int &segs, const int &lenRem) {
+    std::vector<char*> vect;
+    for(int x = 0; x < segs; x++){
+        if(x < segs - 1) {
+            vect.push_back(new char[10]);
+            memcpy(vect[x], memblock + x, 10);
+        }else{
+            if(lenRem > 0){
+                vect.push_back(new char[lenRem]);
+                memcpy(vect[x], memblock + x, lenRem);
+            }else{
+                vect.push_back(new char[10]);
+                memcpy(vect[x], memblock + x, 10);
+            }
+        }
+    }
+    return vect;
+}
 
+char* algo::devectorize(const std::vector<char *> &vect) {
+    char* memblock = new char[((vect.size() - 1) * 10) + strlen(vect[vect.size() - 1])];
+    for(int x = 0; x < vect.size(); x++){
+        memcpy(memblock + x, vect[x], strlen(vect[x]));
+    }
+    return memblock;
+}
--- Algorithms.hpp
+++ Algorithms.hpp
@@ -1,10 +1,19 @@
-//
-// Created by Telepatheticman on 5/20/2019.
-//
-
 #ifndef FAULTYDISK_ALGORITHMS_HPP
 #define FAULTYDISK_ALGORITHMS_HPP
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cstring>
 
+namespace algo{
+    std::vector <char*> vectorize (char* memblock, const int& segs, const int& lenRem);
+
+    char* devectorize (const std::vector<char*>& vect);
+
+    char* vote (const std::vector<char*>& vect);
+}
 
 #endif //FAULTYDISK_ALGORITHMS_HPP
