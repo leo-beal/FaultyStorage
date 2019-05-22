a8e729548eb6624ec24fbe7d58ec14a29a007d50
telepatehticman <telepatheticman13@gmail.com>
2019-05-21 21:50:57 -0600

    test voting and discrepancy checking

--- testMain.cpp
+++ testMain.cpp
@@ -5,70 +5,28 @@
 #include "Algorithms.hpp"
 
 int main(){
-    int sockfd;
-    char buffer[MAXLINE];
-    std::string what("ur mom gay");
-    auto msg = util::createWrite("test.txt", 0, 10, what.c_str());
-    struct sockaddr_in servaddr;
 
+    std::string t1 = "aaaaaaaaaa";
+    std::string t2 = "aaaaaaaaaa";
+    std::string t3 = "aaaaaaaaaa";
 
+    //char* t3prime = (char*)t3.c_str();
+    //t3prime[7] = 0;
+    //t3 = t3prime;
 
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-/*
+    std::vector<unsigned char*> test;
 
+    test.push_back((unsigned char*)t1.c_str());
+    test.push_back((unsigned char*)t2.c_str());
+    test.push_back((unsigned char*)t3.c_str());
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    //char t =
 
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT_OUT);
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    //std::string t = algo::vote(test);
 
+    std::cout << algo::discrep(test, 10) << std::endl;
 
+    std::cout << algo::vote(test, 10) << " == " << strlen(algo::vote(test, 10)) <<  std::endl;
 
-    sendto(sockfd, (const char *)msg, W_LEN,
-           0, (const struct sockaddr *) &servaddr,
-           sizeof(servaddr));
-    printf("Write message sent.\n");
-
-    close(sockfd);
-
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    */
-
-    int n, len;
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT_IN);
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        perror("bind failed");
-        close(sockfd);
-        return 0;
-    }
-    for(int i = 0; i < 10000; i++) {
-        n = recv(sockfd, (char *) buffer, MAXLINE,
-                     0);
-
-        std::cout << n << std::endl << std::flush;
-        std::cout << i << std::endl << std::flush;
-        if (n >= 0){
-            close(sockfd);
-            return 0;
-        }
-    }
-
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
-
-    close(sockfd);
     return 0;
 }
